98a7aaee214ba577d2b60e8a6acbb81d782942ba
Ninja_zakaria <127061000+Cy0x1337@users.noreply.github.com>
2023-03-07 12:33:33 +0100

    Add files via upload

---
+++ admin-main/admin.py
@@ -0,0 +1,246 @@
+import requests
+import os
+import  webbrowser
+os.system("clear")
+
+#======== color ======#
+la7mar  = '\033[91m'
+lazra9  = '\033[94m'
+la5dhar = '\033[92m'
+movv    = '\033[95m'
+lasfar  = '\033[93m'
+ramadi  = '\033[90m'
+blid    = '\033[1m'
+star    = '\033[4m'
+bigas   = '\033[07m'
+bigbbs  = '\033[27m'
+hell    = '\033[05m'
+saker   = '\033[25m'
+labyadh = '\033[00m'
+cyan    = '\033[0;96m'
+#======== color ======#
+
+print("""\033[0;96m
+░█████╗░██████╗░███╗░░░███╗██╗███╗░░██╗  ░██╗░░░░░░░██╗██████╗░
+██╔══██╗██╔══██╗████╗░████║██║████╗░██║  ░██║░░██╗░░██║██╔══██╗
+███████║██║░░██║██╔████╔██║██║██╔██╗██║  ░╚██╗████╗██╔╝██████╔╝
+██╔══██║██║░░██║██║╚██╔╝██║██║██║╚████║  ░░████╔═████║░██╔═══╝░
+██║░░██║██████╔╝██║░╚═╝░██║██║██║░╚███║  ░░╚██╔╝░╚██╔╝░██║░░░░░
+╚═╝░░╚═╝╚═════╝░╚═╝░░░░░╚═╝╚═╝╚═╝░░╚══╝  ░░░╚═╝░░░╚═╝░░╚═╝░░░░░
+            Channel Telegram : @Cy0x_Nj
+""")
+webbrowser.open("https://t.me/Cy0x_Nj")
+url = input("\033[90m╔═══[root@Cy0x_Nj_HackerD]\n╚══>>>   \033[32m")
+print("\n\n")
+lists = ['/wp-login.php','/admin/','/wp-config.php','/admin.php','/cpanal','/cpanal','','/wp-content/plugins/wp-useronline/','/wp-content/plugins/imagemagick-engine/','/wp-content/plugins/3dady-real-time-web-stats/','/admin/index.php/','/phpmyadmin/',
+'/PMA/',
+':2082/',
+'/admin/',
+'/dbadmin/',
+'/mysql/',
+'/myadmin/',
+'/phpmyadmin2/',
+'/phpMyAdmin2/',
+'/phpMyAdmin-2/',
+'/php-my-admin/',
+'/phpMyAdmin-2.2.3/',
+'/phpMyAdmin-2.2.6/',
+'/phpMyAdmin-2.5.1/',
+'/phpMyAdmin-2.5.4/',
+'/phpMyAdmin-2.5.5-rc1/',
+'/phpMyAdmin-2.5.5-rc2/',
+'/phpMyAdmin-2.5.5/',
+'/phpMyAdmin-2.5.5-pl1/',
+'/phpMyAdmin-2.5.6-rc1/',
+'/phpMyAdmin-2.5.6-rc2/',
+'/phpMyAdmin-2.5.6/',
+'/phpMyAdmin-2.5.7/',
+'/phpMyAdmin-2.5.7-pl1/',
+'/phpMyAdmin-2.6.0-alpha/',
+'/phpMyAdmin-2.6.0-alpha2/',
+'/phpMyAdmin-2.6.0-beta1/',
+'/phpMyAdmin-2.6.0-beta2/',
+'/phpMyAdmin-2.6.0-rc1/',
+'/phpMyAdmin-2.6.0-rc2/',
+'/phpMyAdmin-2.6.0-rc3/',
+'/phpMyAdmin-2.6.0/',
+'/phpMyAdmin-2.6.0-pl1/',
+'/phpMyAdmin-2.6.0-pl2/',
+'/phpMyAdmin-2.6.0-pl3/',
+'/phpMyAdmin-2.6.1-rc1/',
+'/phpMyAdmin-2.6.1-rc2/',
+'/phpMyAdmin-2.6.1/',
+'/phpMyAdmin-2.6.1-pl1/',
+'/phpMyAdmin-2.6.1-pl2/',
+'/phpMyAdmin-2.6.1-pl3/',
+'/phpMyAdmin-2.6.2-rc1/',
+'/phpMyAdmin-2.6.2-beta1/',
+'/phpMyAdmin-2.6.2-rc1/',
+'/phpMyAdmin-2.6.2/',
+'/phpMyAdmin-2.6.2-pl1/',
+'/phpMyAdmin-2.6.3/',
+'/phpMyAdmin-2.6.3-rc1/',
+'/phpMyAdmin-2.6.3/',
+'/phpMyAdmin-2.6.3-pl1/',
+'/phpMyAdmin-2.6.4-rc1/',
+'/phpMyAdmin-2.6.4-pl1/',
+'/phpMyAdmin-2.6.4-pl2/',
+'/phpMyAdmin-2.6.4-pl3/',
+'/phpMyAdmin-2.6.4-pl4/',
+'/phpMyAdmin-2.6.4/',
+'/phpMyAdmin-2.7.0-beta1/',
+'/phpMyAdmin-2.7.0-rc1/',
+'/phpMyAdmin-2.7.0-pl1/',
+'/phpMyAdmin-2.7.0-pl2/',
+'/phpMyAdmin-2.7.0/',
+'/phpMyAdmin-2.8.0-beta1/',
+'/phpMyAdmin-2.8.0-rc1/',
+'/phpMyAdmin-2.8.0-rc2/',
+'/phpMyAdmin-2.8.0/',
+'/phpMyAdmin-2.8.0.1/',
+'/phpMyAdmin-2.8.0.2/',
+'/phpMyAdmin-2.8.0.3/',
+'/phpMyAdmin-2.8.0.4/',
+'/phpMyAdmin-2.8.1-rc1/',
+'/phpMyAdmin-2.8.1/',
+'/phpMyAdmin-2.8.2/',
+'/sqlmanager/',
+'/mysqlmanager/',
+'/p/m/a/',
+'/PMA2005/',
+'/pma2005/',
+'/phpmanager/',
+'/php-myadmin/',
+'/phpmy-admin/',
+'/webadmin/',
+'/sqlweb/',
+'/websql/',
+'/webdb/',
+'/mysqladmin/',
+'/mysql-admin/',
+'/private.php/',
+'/robots.txt/',
+'/photoalbum/upload/',
+'/_vti_pvt/',
+':5800/',
+'/phpMyAdmin/',
+'/config.html/',
+'/_private/',
+'admin1.php',
+'admin1.html',
+'admin2.php',
+'admin2.html',
+'yonetim.php',
+'yonetim.html',
+'yonetici.php',
+'yonetici.html',
+'adm/',
+'admin/',
+'admin/account.php',
+'admin/account.html',
+'admin/index.php',
+'admin/index.html',
+'admin/login.php',
+'admin/login.html',
+'admin/home.php',
+'admin/controlpanel.html',
+'admin/controlpanel.php',
+'admin.php',
+'admin.html',
+'admin/cp.php',
+'admin/cp.html',
+'cp.php',
+'cp.html',
+'administrator/',
+'administrator/index.html',
+'administrator/index.php',
+'administrator/login.html',
+'administrator/login.php',
+'administrator/account.html',
+'administrator/account.php',
+'administrator.php',
+'administrator.html',
+'login.php',
+'login.html',
+'modelsearch/login.php',
+'moderator.php',
+'moderator.html',
+'moderator/login.php',
+'moderator/login.html',
+'moderator/admin.php',
+'moderator/admin.html',
+'moderator/',
+'account.php',
+'account.html',
+'controlpanel/',
+'controlpanel.php',
+'controlpanel.html',
+'admincontrol.php',
+'admincontrol.html',
+'adminpanel.php',
+'adminpanel.html',
+'admin1.asp',
+'admin2.asp',
+'yonetim.asp',
+'yonetici.asp',
+'admin/account.asp',
+'admin/index.asp',
+'admin/login.asp',
+'admin/home.asp',
+'admin/controlpanel.asp',
+'admin.asp',
+'admin/cp.asp',
+'cp.asp',
+'administrator/index.asp',
+'administrator/login.asp',
+'administrator/account.asp',
+'administrator.asp',
+'login.asp',
+'modelsearch/login.asp',
+'moderator.asp',
+'moderator/login.asp',
+'moderator/admin.asp',
+'account.asp',
+'controlpanel.asp',
+'admincontrol.asp',
+'adminpanel.asp',
+'fileadmin/',
+'fileadmin.php',
+'fileadmin.asp',
+'fileadmin.html',
+'administration/',
+'administration.php',
+'administration.html',
+'sysadmin.php',
+'sysadmin.html',
+'phpmyadmin/',
+'myadmin/',
+'sysadmin.asp',
+'sysadmin/',
+'ur-admin.asp',
+'ur-admin.php',
+'ur-admin.html',
+'ur-admin/',
+'Server.php',
+'Server.html',
+] 
+
+for inn in lists:
+    try:
+      target_url = f"{url}{inn}"
+      response = requests.get(target_url)
+      if response.status_code == 200:
+          print (f"\033[32m[+] {url}{inn} ==> Found")
+          sami = open("sites.txt","a")
+          sami.write(f"Ok : {url}{inn}")
+          sami.close()
+      else:
+          print (f"\033[31m[-] {url}{inn} ==> Not Found")
+      print("\n\n\033[0;96m[!] Save The Site File Name ==:> sites.txt")
+    except:
+      print("Error The Site https:// or http://")
+      slact = input("Exit write (e) restart write (y) ?")
+      if slact == "y":
+          os.system("python admin.py")
+          print("Exit Now ...")
+os.system("exit")
---
+++ zakaria.py
@@ -0,0 +1,2 @@
+url = input("\033[90m╔═══[root@Cy0x_Nj_HackerD]\n╚══>>>   \033[32m")
+print("\n\n")
